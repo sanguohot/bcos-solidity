8b754592704f2ce9998aadf7a8e36c00c701976b
evan <281406674@qq.com>
2018-07-05 15:22:47 +0800

    增加用户和文件数组 方便后续导出
    Signed-off-by: evan <281406674@qq.com>

--- FilesPureData.sol
+++ FilesPureData.sol
@@ -14,7 +14,11 @@ contract FilesPureData is ContractBase("v2") {
         bytes32[4] detail;
         uint time;
     }
+    // 文件map
     mapping(bytes32=>Files) filesMap;
+    // 文件列表 方便以后导出 只增不减 注意唯一性
+    // 这里不考虑普通索引的问题 使用普通索引请先导入MongoDB或者关系型数据库进行检索
+    bytes32[] filesList;
 
     event onSetFilesPureDataInvokerAddress(address invokerAddress);
 
@@ -24,10 +28,33 @@ contract FilesPureData is ContractBase("v2") {
     }
 
     function setInvoker(address invokerAddress) public{
-      if(msg.sender == owner){
+      if(msg.sender==owner && invokerAddress!=0){
         invoker = invokerAddress;
         onSetFilesPureDataInvokerAddress(invoker);
       }
+    }
+
+    function getFilesListSize() public constant returns (uint){
+      return filesList.length;
+    }
+    function getFileIdInFilesList(uint index) public constant returns (bytes32){
+      if(msg.sender==invoker || msg.sender==owner){
+        if(index>=0 && index<filesList.length){
+          return filesList[index];
+        }
+      }
+      return 0;
+    }
+
+    function setfileIdToFilesList(bytes32 fileId) private returns (bool){
+      if(msg.sender==invoker || msg.sender==owner){
+        if(filesMap[fileId].time==0){
+          filesList.push(fileId);
+          return true;
+        }
+        return false;
+      }
+      return false;
     }
 
     function getActiveInFilesMap(bytes32 fileId) public constant returns (bool){
@@ -163,6 +190,25 @@ contract FilesPureData is ContractBase("v2") {
       return valid;
     }
 
+    function isSignDataExist(bytes32 fileId, bytes32[4] signData) public constant returns (bool valid){
+      valid = false;
+      if(msg.sender==invoker || msg.sender==owner){
+        for(uint i=0; i<getFileSignSize(fileId); i++){
+          bytes32[4] memory signDataInList = filesMap[fileId].signDataList[i];
+          if(
+              signDataInList[0]==signData[0] &&
+              signDataInList[1]==signData[1] &&
+              signDataInList[2]==signData[2] &&
+              signDataInList[3]==signData[3]
+          ){
+            valid = true;
+            break;
+          }
+        }
+      }
+      return valid;
+    }
+
     function getFileSignDataByIndex(bytes32 fileId, uint index) public constant returns (bytes32[4]) {
       if(msg.sender==invoker || msg.sender==owner){
         return filesMap[fileId].signDataList[index];
@@ -172,7 +218,7 @@ contract FilesPureData is ContractBase("v2") {
 
     function addFileSigner(bytes32 fileId, address userId, bytes32[4] signData) public returns (bool succ){
       if(msg.sender==invoker || msg.sender==owner){
-        if(!isUserIdExist(fileId, userId)){
+        if(!isUserIdExist(fileId, userId) && !isSignDataExist(fileId, signData)){
           filesMap[fileId].signerAddressList.push(userId);
           filesMap[fileId].signDataList.push(signData);
           return true;
@@ -184,6 +230,7 @@ contract FilesPureData is ContractBase("v2") {
     function addFile(bytes32 fileId, address userId, bytes32[4] signData, bytes32[2] fileHash, bytes32[2] ipfsHash, bytes32[4] detail, uint time) public returns (bytes32){
       if(msg.sender==invoker || msg.sender==owner){
         if(!filesMap[fileId].active){
+            setfileIdToFilesList(fileId);
             address[] memory signerAddressList = new address[](1);
             bytes32[4][] memory signDataList = new bytes32[4][](1);
             signerAddressList[0] = userId;
--- UsersPureData.sol
+++ UsersPureData.sol
@@ -17,6 +17,9 @@ contract UsersPureData is ContractBase("v2") {
     mapping(address=>Users) usersMap;
     // idCartNo => userId(address) 控制idCartNo唯一
     mapping(bytes32=>address) addressMap;
+    // 保存用户列表 方便以后导出 只增不减 注意唯一性
+    // 这里不考虑普通索引的问题 使用普通索引请先导入MongoDB或者关系型数据库进行检索
+    address[] usersList;
 
     event onSetUsersPureDataInvokerAddress(address invokerAddress);
 
@@ -26,10 +29,33 @@ contract UsersPureData is ContractBase("v2") {
     }
 
     function setInvoker(address invokerAddress) public{
-      if(msg.sender == owner){
+      if(msg.sender==owner && invokerAddress!=0){
         invoker = invokerAddress;
         onSetUsersPureDataInvokerAddress(invoker);
       }
+    }
+
+    function getUsersListSize() public constant returns (uint){
+      return usersList.length;
+    }
+    function getUserIdInUsersList(uint index) public constant returns (address){
+      if(msg.sender==invoker || msg.sender==owner){
+        if(index>=0 && index<usersList.length){
+          return usersList[index];
+        }
+      }
+      return 0;
+    }
+
+    function setUserIdToUsersList(address userId) private returns (bool){
+      if(msg.sender==invoker || msg.sender==owner){
+        if(userId!=0 && usersMap[userId].time==0){
+          usersList.push(userId);
+          return true;
+        }
+        return false;
+      }
+      return false;
     }
 
     function getUserIdInAddressMap(bytes32 idCartNo) public constant returns (address){
@@ -39,10 +65,10 @@ contract UsersPureData is ContractBase("v2") {
       return 0;
     }
 
-    function setUserIdToAddressMap(bytes32 idCartNo, address accountAddress) public returns (bool){
+    function setUserIdToAddressMap(bytes32 idCartNo, address userId) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(addressMap[idCartNo]==0){
-          addressMap[idCartNo] = accountAddress;
+        if(userId!=0 && addressMap[idCartNo]==0){
+          addressMap[idCartNo] = userId;
           return true;
         }
         return false;
@@ -59,7 +85,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setActiveToUsersMap(address userId, bool active) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].time!=0){
+        if(userId!=0 && usersMap[userId].time!=0){
           usersMap[userId].active = active;
           return true;
         }
@@ -77,7 +103,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setAccountAddressToUsersMap(address userId, address accountAddress) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].active){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].accountAddress = accountAddress;
           return true;
         }
@@ -95,7 +121,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setPublicKeyToUsersMap(address userId, bytes32[2] publicKey) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].time!=0){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].publicKey = publicKey;
           return true;
         }
@@ -113,7 +139,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setIdCartNoToUsersMap(address userId, bytes32 idCartNo) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].active){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].idCartNo = idCartNo;
           return true;
         }
@@ -140,7 +166,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setDetailToUsersMap(address userId, bytes32[8] detail) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].active){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].detail = detail;
           return true;
         }
@@ -158,7 +184,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function setTimeToUsersMap(address userId, uint time) public returns (bool){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].active){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].time = time;
           return true;
         }
@@ -170,7 +196,8 @@ contract UsersPureData is ContractBase("v2") {
     function addUserToUsersMap(address userId,address accountAddress,bytes32[4] publicKey,bytes32 idCartNo
     ,bytes32[8] detail,uint time) public returns (address){
       if(msg.sender==invoker || msg.sender==owner){
-        if(!usersMap[userId].active){
+        if(userId!=0 && !usersMap[userId].active){
+          setUserIdToUsersList(userId);
           usersMap[userId] = Users(true,accountAddress,publicKey,idCartNo,detail,time);
           addressMap[idCartNo] = userId;
           return userId;
@@ -182,7 +209,7 @@ contract UsersPureData is ContractBase("v2") {
 
     function delUserInUsersMap(address userId) public returns (bool succ){
       if(msg.sender==invoker || msg.sender==owner){
-        if(usersMap[userId].active){
+        if(userId!=0 && usersMap[userId].active){
           usersMap[userId].active = false;
           addressMap[usersMap[userId].idCartNo] = 0;
           return true;
